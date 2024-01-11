a7aa58b20e5244a49c398ce9dc950f47d5bc00be
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2024-01-11 11:54:55 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -72,7 +72,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 text="ᴏᴡɴᴇʀ", url="https://t.me/@lll_THUNDERXD_lll",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FAKE_SMILE_76",
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FAKE_SMILE_78",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -91,10 +91,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_THUNDERXD_ll",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/lll_THUNDERXD_lll",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FAKE_SMILE_76",
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/FAKE_SMILE_78",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
